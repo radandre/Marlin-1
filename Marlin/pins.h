3f9d72ce199364f07de206b2e827cb65eff76605
radandre <radandre@yahoo.com>
2013-12-27 23:05:36 +0100

    Update pins.h
    update rumba pins for fan_pin2 function

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -1343,6 +1343,8 @@
 
 #define FAN_PIN            7
 //additional FAN1 PIN (e.g. useful for electronics fan or light on/off) on PIN 8
+#define FAN_PIN2           8 // IO pin. Buffer needed
+#define FAN_TO_FAN2 // If enabled FAN_PIN2 acts exactly like FAN_PIN
 
 #define PS_ON_PIN          45
 #define KILL_PIN           46
