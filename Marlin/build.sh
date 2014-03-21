728844e11c6468132ea1bbf1a366d4a6a4c24918
Cristian Giussani <giussani@fastflow.it>
2014-03-21 15:56:37 +0100

    Added extrusion before change filament

--- Marlin/build.sh
+++ Marlin/build.sh
@@ -4,7 +4,8 @@ export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 VERSION="1.0.0"
 DATE=$(date +%Y%m%d)
 
-rm ../firmware.xml
+#rm ../firmware.xml
+echo "<firmwares>" > ../firmware.xml
 
 for hw in $(echo "34 80"); do
    for lang in $(echo "1 6 7"); do
@@ -35,18 +36,25 @@ for hw in $(echo "34 80"); do
             ;;
       esac
 
+      # Hardware
+      case "$hw" in
+         34) BOARD_ID="0"
+             ;;
+         80) BOARD_ID="1"
+             ;;
+      esac
+
       cat << END >> ../firmware.xml
 <firmware>
         <Version>$VERSION</Version>
         <Description>$released.</Description>
-        <link board="0" ext="1" bed="1" lang="$lang_code">
-                http://www.sharebot.eu/downloads/firmware/fw-H$hw-L$lang-$VERSION_$DATE-11.hex
-        </link>
-        <link board="0" ext="2" bed="1" lang="$lang_code">
-                http://www.sharebot.eu/downloads/firmware/fw-H$hw-L$lang-$VERSION_$DATE-21.hex
-        </link>
-</firmware>"
+        <link board="$BOARD_ID" ext="1" bed="1" lang="$lang_code">http://www.sharebot.eu/downloads/firmware/fw-H$hw-L$lang-$VERSION_$DATE-11.hex</link>
+        <link board="$BOARD_ID" ext="2" bed="1" lang="$lang_code">http://www.sharebot.eu/downloads/firmware/fw-H$hw-L$lang-$VERSION_$DATE-21.hex</link>
+</firmware>
 END
    done
 done
+
+echo "</firmwares>" >> ../firmware.xml
+
 exit
